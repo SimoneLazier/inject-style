4afd7d01cc9e0affb4a0265f1e3a4c609596ef1a
Simone Lazier <info@jswebagency.com>
2021-09-08 16:16:20 +0200

    Ignore ms prefix rules

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
 export default (...css: string[]) => {
-  const sheet = new CSSStyleSheet()
+  const sheet = new CSSStyleSheet();
   css
-    .join('')
-    .replaceAll(/(\/\*.*?\*\/)|\s/gs, '')
-    .split('}')
-    .filter((rule) => !rule.includes('-moz-') && rule.trim() !== '')
-    .map((rule) => rule + '}')
-    .forEach((rule) => sheet.insertRule(rule))
+    .join("")
+    .replaceAll(/(\/\*.*?\*\/)|\s/gs, "")
+    .split("}")
+    .filter(
+      (rule) =>
+        !rule.includes("-moz-") && !rule.includes("-ms-") && rule.trim() !== ""
+    )
+    .map((rule) => rule + "}")
+    .forEach((rule) => sheet.insertRule(rule));
 
-  type WebComponent = typeof HTMLElement
+  type WebComponent = typeof HTMLElement;
 
   return (component: WebComponent): WebComponent => {
     return class extends component {
       constructor() {
-        super()
+        super();
         // @ts-ignore
-        this.shadowRoot.adoptedStyleSheets = [sheet]
+        this.shadowRoot.adoptedStyleSheets = [sheet];
       }
-    }
-  }
-}
+    };
+  };
+};
